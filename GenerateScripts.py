5b68155b72390083f218c6f5a300be4d4fcfe166
Abraham Albert <abrahamalbert18@icloud.com>
2024-02-28 22:54:31 +1100

    Updated README.md based on the latest model outputs.

--- GenerateScripts.py
+++ GenerateScripts.py
@@ -5,13 +5,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--modelName",
-                    default=f"Transformer-->ScriptWriter-->v1.pth.tar")
+                    default=f"Encoder-->ScriptWriter-->v2.pth.tar")
 parser.add_argument("-nv", "--cuda", default=False, type=bool)
 parser.add_argument("-t", "--tokens", default=500, type=int)
 parser.add_argument("-w", "--word", default="", type=str)
 parser.add_argument("-v", "--vocabSize", default=30000, type=int)
-parser.add_argument("-cl", "--contextLength", default=512, type=int)
-parser.add_argument("-d", "--depth", default=8, type=int)
+parser.add_argument("-cl", "--contextLength", default=768, type=int)
+parser.add_argument("-d", "--depth", default=4, type=int)
 args = parser.parse_args()
 
 modelName = args.modelName
@@ -59,21 +59,29 @@ model.load_state_dict(checkpoint["modelStateDict"])
 model.eval()
 
 softmax = torch.nn.Softmax(dim=-1)
-
+# predictedWords = ""
 print(f"{'-'*40}\n\n")
+# newSource = torch.zeros(numberOfTokens // 100, maxSequenceLength - 1)
+# newTarget = torch.zeros(numberOfTokens // 100, maxSequenceLength - 1)
+memory = None
+
 for l in range(numberOfTokens):
     if source.size(-1) > (maxSequenceLength - 1):
-        words = tokenizer.decode(target[:-1].short().tolist())
-        # predictedWords.append(words)
+        if l < 100:
+            words = tokenizer.decode(target[:-1].short().tolist())
+            memory = 1
+        else:
+            memory = 20
+            words = tokenizer.decode(target[memory:-1].short().tolist())
+
         print(words)
-        source = source[-1:]
-        target = target[-1:]
+        # predictedWords.append(words)
+        # predictedWords += words + " "
+        source = source[-1 * memory:]
+        target = target[-1 * memory:]
 
     outputs = model(source.unsqueeze(0), target.unsqueeze(0)) # Encoder-Decoder
     nextTokenProbs = softmax(outputs[-1])
-    #print("Actual Ouptuts:")
-    #print(tokenizer.decode((torch.multinomial(outputs.softmax(dim=-1), num_samples=1)).reshape(-1).tolist()))
-    #print()
     predictions = torch.multinomial(nextTokenProbs,
                                     num_samples=1).to("cpu")
     #predictions = torch.argmax(nextTokenProbs).to("cpu").unsqueeze(0)
@@ -82,3 +90,26 @@ for l in range(numberOfTokens):
     target = torch.cat((target, predictions))
 
 print(f"{'-'*40}")
+
+def generate(source):
+
+    numberOfTokens = maxSequenceLength - 1 - source.size(-1)
+    newSource = torch.zeros((1, numberOfTokens)).short()
+
+    for j in range(5):
+        for i in range(numberOfTokens):
+            outputs = model(source.unsqueeze(0), target.unsqueeze(0)) # Encoder-Decoder
+            nextTokenProbs = softmax(outputs[-1])
+            predictions = torch.multinomial(nextTokenProbs,
+                                            num_samples=1).to("cpu")
+            source = torch.cat((source, predictions))
+            target = torch.cat((target, predictions))
+        newSource = torch.cat((newSource, source.unsqueeze(0)), dim=0)
+        source = newSource[1:]
+    words = tokenizer.decode(target[:-1].short().tolist())
+    print(words)
+
+
+
+
+# print(predictedWords)
